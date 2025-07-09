16a27b2d2c45b6e4fa2787fda11f3954f7f285b9
loice okello <loicelaura@gmail.com>
2025-07-09 11:23:09 +0300

    updated task_1

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,31 +1,68 @@
-interface Teacher{
+interface Teacher {
     firstName: string;
     lastName: string;
     fullTimeEmployee?: boolean;
-    yearsOfExperience:number;
+    yearsOfExperience: number;
     location: string;
     [propName: string]: any; // allow additional properties
-
 }
-//examples
-const teacher1: Teacher ={
+
+interface Director extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+const teacher1: Teacher = {
     firstName: "Mercy",
-    lastName:"akinyi",
-    fullTimeEmployee:true,
-    yearsOfExperience:5,
-    location:"mombasa",
+    lastName: "Akinyi",
+    fullTimeEmployee: true,
+    yearsOfExperience: 5,
+    location: "Mombasa",
     contract: false,
 };
 
-const teacher2: Teacher ={
-     firstName: "winny",
-    lastName:"akinyi",
-    fullTimeEmployee:true,
-    yearsOfExperience:3,
-    location:"mombasa",
-    contract: false,
-    specialization:"mathematics",
+const director1: Director = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    numberOfReports: 17,
 };
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
 
 console.log(teacher1);
-console.log(teacher2);
+console.log(director1);
+console.log(printTeacher("John", "Doe"));
+console.log(printTeacher("Mercy", "Akinyi")); 
+console.log(student.displayName());  
+console.log(student.workOnHomework()); 
