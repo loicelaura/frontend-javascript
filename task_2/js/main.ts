03d33628cb475d50e864639c020bc28229e267fb
loice okello <loicelaura@gmail.com>
2025-07-09 13:27:21 +0300

    updated task_2

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -41,7 +41,33 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+// The type predicate function
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+}
+
+// Function to execute appropriate work
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+ type Subjects ='Math'|'History';
+ function teachClass(todayClass:Subjects): string  {
+    if(todayClass === 'Math') {
+        return 'Teaching Math';
+    }else {
+        return 'Teaching History';
+    }
+ }
+
 // Example usage:
 console.log(createEmployee(200).workFromHome()); // Cannot work from home
 console.log(createEmployee(1000).workFromHome()); // Working from home
 console.log(createEmployee('$5000').getCoffeeBreak()); // Getting a coffee break
+console.log(executeWork(createEmployee(200)));  // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
